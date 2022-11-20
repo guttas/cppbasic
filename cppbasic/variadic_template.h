bb8c83f8d8ca381c7498e95aec527385d5a18c1f
Tao Guo <guttas@163.com>
2022-11-20 16:48:34 +0800

    Modify parameter names and add ignore files

--- cppbasic/variadic_template.h
+++ cppbasic/variadic_template.h
@@ -3,22 +3,21 @@
 void print_cpp11()
 {}
 
-template<typename T, typename... ARGS>
-void print_cpp11(T first, ARGS... types)
+template<typename T, typename... Types>
+void print_cpp11(T firstArg, Types... args)
 {
-	std::cout << first << std::endl;
+	std::cout << firstArg << std::endl;
 
-	print_cpp11(types...);
+	print_cpp11(args...);
 }
 
-template<typename T, typename... ARGS>
-void print_cpp17(T first, ARGS... types)
+template<typename T, typename... Types>
+void print_cpp17(T firstArg, Types... args)
 {
-	std::cout << first << std::endl;
+	std::cout << firstArg << std::endl;
 
-	if constexpr (sizeof...(types) > 0)
+	if constexpr (sizeof...(args) > 0)
 	{
-		print_cpp17(types...);
+		print_cpp17(args...);
 	}
-
 }
